6c671f4aa730d9a318140e165d1b07c740108fe1
jambox <nonya@business.com>
2023-12-27 21:06:20 -0800

    feat(square picking): can now successfully tell which square we have picked

--- board.py
+++ board.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 from collections import namedtuple
 
-Point = lambda x,y : namedtuple('Point', 'x y')(x,y)
+# import pprint
+# pp = pprint.PrettyPrinter(indent=4)
+
+Point = lambda x,y : namedtuple('Point', 'x y')(int(x),int(y))
 Line = lambda p1, p2 : namedtuple('Line', 'p1, p2')(p1, p2)
 
 class Board():
@@ -10,53 +13,91 @@ class Board():
     self.SIZE = 800
     self.CENTER = Point(self.SIZE/2, self.SIZE/2)
 
+    self.lines = {}
+
     self.canvas = tk.Canvas(self.root, width = self.SIZE, height = self.SIZE, bg='white')
-    self.generate_board(self.CENTER, 700)
+    self.generate_board(self.CENTER, 700, "main")
     self.generate_inner_boards(700)
     self.canvas.pack(anchor=tk.CENTER, expand=True)
+    self.root.bind("<Button>", self.click_handler)
     self.root.mainloop()
 
   def generate_inner_boards(self, size):
     # accounts for padding away from wall
     offset = (self.SIZE - size) / 2 + size/6
 
-    self.generate_board(Point(offset, offset), 200)
-    self.generate_board(Point(self.CENTER.x, offset), 200)
-    self.generate_board(Point(self.SIZE - offset, offset), 200)
+    self.generate_board(Point(offset, offset), 200, 'a1')
+    self.generate_board(Point(self.CENTER.x, offset), 200, 'b1')
+    self.generate_board(Point(self.SIZE - offset, offset), 200, 'c1')
 
 
-    self.generate_board(Point(offset, self.CENTER.y ), 200)
-    self.generate_board(Point(self.CENTER.x, self.CENTER.y), 200)
-    self.generate_board(Point(self.SIZE - offset, self.CENTER.y), 200)
+    self.generate_board(Point(offset, self.CENTER.y ), 200, 'a2')
+    self.generate_board(Point(self.CENTER.x, self.CENTER.y), 200, 'b2')
+    self.generate_board(Point(self.SIZE - offset, self.CENTER.y), 200, 'c2')
 
-    self.generate_board(Point(offset, self.SIZE - offset), 200)
-    self.generate_board(Point(self.CENTER.x, self.SIZE - offset), 200)
-    self.generate_board(Point(self.SIZE - offset, self.SIZE - offset), 200)
+    self.generate_board(Point(offset, self.SIZE - offset), 200, 'a3')
+    self.generate_board(Point(self.CENTER.x, self.SIZE - offset), 200, 'b3')
+    self.generate_board(Point(self.SIZE - offset, self.SIZE - offset), 200, 'c3')
 
-
-
-  def generate_board(self, center, size):
+  def generate_board(self, center, size, name):
     l_mod = 6
     s_mod = 2
     c_mod = 70
     w_mod = 175
 
     self.canvas.create_oval((center.x - size / c_mod, center.y -size / c_mod ), (center.x +size / c_mod, center.y + size / c_mod))
-    l1 = Line(Point(center.x - size / l_mod, center.y - size / s_mod), Point(center.x - size / l_mod, center.y + size / s_mod))
-    l2 = Line(Point(center.x + size / l_mod, center.y - size / s_mod), Point(center.x + size / l_mod, center.y + size / s_mod))
-    l3 = Line(Point(center.x - size / s_mod, center.y - size / l_mod), Point(center.x + size / s_mod, center.y - size / l_mod))
-    l4 = Line(Point(center.x - size / s_mod, center.y + size / l_mod), Point(center.x + size / s_mod, center.y + size / l_mod))
+    v1 = Line(Point(center.x - size / l_mod, center.y - size / s_mod), Point(center.x - size / l_mod, center.y + size / s_mod))
+    v2 = Line(Point(center.x + size / l_mod, center.y - size / s_mod), Point(center.x + size / l_mod, center.y + size / s_mod))
+    h1 = Line(Point(center.x - size / s_mod, center.y - size / l_mod), Point(center.x + size / s_mod, center.y - size / l_mod))
+    h2 = Line(Point(center.x - size / s_mod, center.y + size / l_mod), Point(center.x + size / s_mod, center.y + size / l_mod))
 
-    lines = [l1,l2,l3,l4]
-    # for l in self.lines:
-    #   print(f"line at points: {l}")
+    lines = {
+      'v1' : v1,
+      'v2' : v2,
+      'h1' : h1,
+      'h2' : h2,
+    }
 
-    self.canvas.create_line(l1.p1, l1.p2, width = size / w_mod, fill = 'black')
-    self.canvas.create_line(l2.p1, l2.p2, width = size / w_mod, fill = 'black')
+    self.lines[name] = lines
 
-    self.canvas.create_line(l3.p1, l3.p2, width = size / w_mod, fill = 'black')
-    self.canvas.create_line(l4.p1, l4.p2, width = size / w_mod, fill = 'black')
+    self.canvas.create_line(v1.p1, v1.p2, width = size / w_mod, fill = 'black')
+    self.canvas.create_line(v2.p1, v2.p2, width = size / w_mod, fill = 'black')
 
+    self.canvas.create_line(h1.p1, h1.p2, width = size / w_mod, fill = 'black')
+    self.canvas.create_line(h2.p1, h2.p2, width = size / w_mod, fill = 'black')
+
+  def get_board(self, event, board):
+    # print(f"Checking: {event.x}, {event.y} for board: {board}")
+    # pp.pprint(self.lines[board])
+
+    row = None
+    column = None
+
+    # switch stuff # TODO get rid of hardcode its wrong
+    if event.y < self.lines[board]["h1"].p1.y and event.y > self.lines[board]["v1"].p1.y:
+      row = "1"
+    elif event.y > self.lines[board]["h1"].p1.y and event.y < self.lines[board]["h2"].p1.y:
+      row = "2"
+    elif event.y > self.lines[board]["h2"].p1.y and event.y < self.lines[board]["v2"].p2.y:
+      row = "3"
+    else:
+      row = "?"
+
+    if event.x < self.lines[board]["v1"].p1.x and event.x > self.lines[board]["h1"].p1.x:
+      column = "a"
+    elif event.x > self.lines[board]["v1"].p1.x and event.x < self.lines[board]["v2"].p1.x:
+      column = "b"
+    elif event.x > self.lines[board]["v2"].p1.x and event.x < self.lines[board]["h2"].p2.x:
+      column = "c"
+    else:
+      column = "?"
+
+    return column + row
+
+  def click_handler(self, event):
+    mini_board = self.get_board(event, "main")
+    square = self.get_board(event, mini_board)
+    print(mini_board + square)
 
 if __name__ == "__main__":
-  Board()
+  b = Board()
