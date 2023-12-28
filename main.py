9ec08b7cffa7f457c0711bf3b86c3ccc77e749dc
jambox <nonya@business.com>
2023-12-28 13:03:40 -0800

    some refactoring...

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
+import signal
+import threading
+import time
+
 from server import PiecePlacer
 from board import Board
 
 class MetaTicTacToe():
   def __init__(self):
-    # self.board = Board()
-    self.server = PiecePlacer()
-    self.server.serve()
 
+    # spawn board
+    self.board_thread = threading.Thread(target=Board)
+
+    #start server
+    self.peice_place_cv = threading.Condition()
+    self.server = PiecePlacer(self.peice_place_cv )
+
+    #start workthread
+    self.do_work = False
+    self.work_thread = threading.Thread(target=self.work_func, args=())
+
+  def work_func(self):
+    while(self.do_work):
+      print("we are wokring")
+      with self.peice_place_cv:
+        self.peice_place_cv.wait()
+
+    print("exit")
+
+  def start_work(self):
+    if (self.do_work is False):
+      self.do_work = True
+      self.board_thread.start()
+      self.work_thread.start()
+    else:
+      print("WARNING: already doing work")
+      return
+
+  def stop_work(self, *args):
+    print("AHHH")
+    if (self.do_work is True):
+      self.do_work = False
+      self.work_thread.join()
+    else:
+      print("WARNING: not doing any work, meaningless call")
+      return
 
 if __name__ == "__main__":
-  MetaTicTacToe()
+  m = MetaTicTacToe()
+  signal.signal(signal.SIGINT, m.stop_work)
+  m.start_work()
+  '''
+  # I really don't like that his has to be last but we'll leave it forx
+  # Something with the ThreadPoolExector does not liked to be spawned within its own thread
+  '''
+  m.server.serve() 
--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from concurrent import futures
 import logging
+import queue
+import threading
+import signal
+import time
 
 import grpc
 
@@ -7,18 +11,51 @@ from remote_calls.game_pb2_grpc import PiecePlacerServicer, add_PiecePlacerServi
 from remote_calls.game_pb2 import Reply, PlayerChoice
 
 class PiecePlacer(PiecePlacerServicer):
+  def __init__(self, cond_var = threading.Condition()):
+    self.place_request_queue = queue.Queue(maxsize = 81) # queue of player choices
+    self.cv = cond_var
+
+    self.do_work = False
+    self.thread_pool = futures.ThreadPoolExecutor(max_workers=10)
+    self.work_thread = threading.Thread(target=self.serve)
+
   def ChooseSquare(self, request, context):
+    self.place_request_queue.put(request)
+    with self.cv:
+      self.cv.notify()
     return Reply(message=f"Player {str(request.piece)} requested place on square {request.square}")
 
   def serve(self):
       port = "50051"
-      self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-      add_PiecePlacerServicer_to_server(PiecePlacer(), self.server)
+      self.server = grpc.server(self.thread_pool)
+      add_PiecePlacerServicer_to_server(self, self.server)
       self.server.add_insecure_port("[::]:" + port)
       self.server.start()
       print("Server started, listening on " + port)
       self.server.wait_for_termination()
+      print("exitted")
+
+  def start_work(self):
+    if (self.do_work is False):
+      self.do_work = True
+      self.work_thread.start()
+    else:
+      print("WARNING: already doing work")
+      return
+  
+  def stop_work(self, *args):
+    if (self.do_work is True):
+      self.do_work = False
+      self.work_thread.join()
+    else:
+      print("WARNING: not doing any work, meaningless call")
+      return
 
 if __name__ == "__main__":
     logging.basicConfig()
-    PiecePlacer().serve()
+    p = PiecePlacer()
+    p.serve()
+    while(True):
+      time.sleep(5)
+    # p.start_work()
+
