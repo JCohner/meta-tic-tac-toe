ccc8ee5b14e3ac6fefe14699937d7a1963df4688
jambox <nonya@business.com>
2023-12-29 12:31:11 -0800

    feat(exes and ohs): got it!

--- board.py
+++ board.py
@@ -7,9 +7,13 @@ import logging
 from pprint import PrettyPrinter, pformat
 pp = PrettyPrinter(indent=4)
 
+'''
+'Operator Overloads'
+'''
 Point = lambda x,y : namedtuple('Point', 'x y')(int(x),int(y))
 Line = lambda p1, p2 : namedtuple('Line', 'p1, p2')(p1, p2)
 Len = lambda L : int(sqrt((L.p1.x - L.p2.x)**2 + (L.p1.y - L.p2.y)**2))
+
 
 Piece = Enum('Piece', ['X', 'O'])
 # TODO(josh): could use list comprehension to make an enum of all valid squares....
@@ -24,7 +28,7 @@ class Board():
     self.centers = {}
 
     self.canvas = tk.Canvas(self.root, width = self.SIZE, height = self.SIZE, bg='white')
-    self.generate_board(self.CENTER, 700, "main")
+    self.generate_board(self.CENTER, 700, "xx")
     self.generate_inner_boards(700)
     logging.debug(pformat(self.lines))
 
@@ -117,18 +121,30 @@ class Board():
           p = Point(x = self.lines[key]['h1'].p1.x + k * offset_to_center.x, 
                     y = self.lines[key]['v1'].p1.y + l * offset_to_center.y)
           temp_d[j+i] = p
-          # if key != "main":
+          # if key != "xx":
           #   self.canvas.create_text(*p, text = key+j+i)
       self.centers[key] = temp_d
 
   def generate_shape(self, shape, square):
     p = self.centers[square[:2]][square[2:]]
+    size_mod = Len(self.lines[square[:2]]['h1']) * 1/4
+    corners = (p.x - size_mod/2, p.y - size_mod/2), (p.x + size_mod/2, p.y + size_mod/2)
+    other_corners = (p.x - size_mod/2, p.y + size_mod/2), (p.x + size_mod/2, p.y - size_mod/2)
+    if (shape == Piece.O):
+      self.canvas.create_oval(*corners, outline='red', width = 4)
+    elif (shape == Piece.X):
+      self.canvas.create_line(*corners, width = 4, fill = 'red')
+      self.canvas.create_line(*other_corners, width = 4, fill = 'red')
+    else:
+      # throw err
+      pass
 
   def click_handler(self, event):
     click_point = Point(event.x, event.y)
-    mini_board = self.get_board(click_point, "main")
+    mini_board = self.get_board(click_point, "xx")
     square = self.get_board(click_point, mini_board)
     print(mini_board + square)
+    self.generate_shape(Piece.X, mini_board + square)
 
 if __name__ == "__main__":
   logging.basicConfig(filename='logs/board.log', encoding='utf-8', level=logging.DEBUG)
