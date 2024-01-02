7ef716cd76d5a7842025241d289dc517a9601cd6
jambox <nonya@business.com>
2024-01-02 11:05:18 -0800

    feat(helpers | SharedEnum): implemented a shared enum type

--- tictac/helpers.py
+++ tictac/helpers.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from enum import Enum
 from math import sqrt
 
+from multiprocessing import Value
 
 '''
 'Operator Overloads'
@@ -10,7 +11,9 @@ Point = lambda x,y : namedtuple('Point', 'x y')(int(x),int(y))
 Line = lambda p1, p2 : namedtuple('Line', 'p1, p2')(p1, p2)
 Len = lambda L : int(sqrt((L.p1.x - L.p2.x)**2 + (L.p1.y - L.p2.y)**2))
 
-
+'''
+Enums
+'''
 #WARNING: keep these enums aligned with the enum Piece as degined in remote_call/game.proto
 Piece = Enum('Piece', ['N', 'X', 'O'])
 
@@ -22,3 +25,21 @@ Square = Enum('Squares',
 Move = namedtuple('Move', 'piece square')
 PlayState = Enum('PlayState', ['IN_PLAY', 'X_WON', 'O_WON', 'TIE'])
 GameState = namedtuple('GameState', 'board play_state')
+
+'''
+Thread (process) safe helpers 
+'''
+class SharedEnum():
+  def __init__(self, enum):
+    self.enum_val = enum
+    self.enum_type = type(enum)
+    self.__safe_val__ = Value('i', enum.value)
+
+  # return value as its enum type
+  def get_value(self):
+    with self.__safe_val__.get_lock():
+      return self.enum_type(self.__safe_val__.value)
+
+  def set_value(self, enum):
+    with self.__safe_val__.get_lock():
+      self.__safe_val__.value = enum.value
